ac1d0917f1b1986dd80d9d9b7866cf451c24fe4b
Claudio-R <claudio98.rc@gmail.com>
2021-11-24 16:00:44 +0100

    initialization, added Vue and Tone packages

---
+++ script.js
@@ -0,0 +1,25 @@
+import Vue from "vue/dist/vue.min"
+import * as Tone from "tone"
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        pads: [true, true, false, true],
+        beatIndex: 0
+    },
+    methods: {
+        toggleActive: function(i) {
+            Vue.set(this.pads, i, !this.pads[i])
+        },
+
+        nextBeat: function() {
+            this.beatIndex = (this.beatIndex + 1) % this.pads.length;
+            if(this.pads[this.beatIndex]) {
+                player.start()
+            }
+        }
+
+    }
+})
+
+global.app = app
