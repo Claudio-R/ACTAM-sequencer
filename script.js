1244759a5a4583da45383e65e594cdabd135c491
Claudio-R <claudio98.rc@gmail.com>
2021-11-24 20:18:27 +0100

    added buttons to add or remove keys, slider to select bpm

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
-import Vue from "node_modules/vue/dist/vue.min.js"
-import * as Tone from "tone"
+import Vue from "/node_modules/vue/dist/vue.min.js";
+import * as Tone from 'tone';
 
-var app = new Vue({
-    el: '#app',
+/* MODEL */
+
+var sequencer = new Vue({
+    
+    el:"#sequencer",
+    
     data: {
-        pads: [true, true, false, true],
-        beatIndex: 0
-    },
+        beat: 0,
+        totalBeats: 8,
+        keys: [false, false, false, false, false, false, false, false],
+        bpm: 180,
+    }, 
+    
     methods: {
-        toggleActive: function(i) {
-            Vue.set(this.pads, i, !this.pads[i])
+        toggleKey: function(index) {
+            Vue.set(this.keys, index, !this.keys[index]);
         },
-
         nextBeat: function() {
-            this.beatIndex = (this.beatIndex + 1) % this.pads.length;
-            if(this.pads[this.beatIndex]) {
-                player.start()
-            }
+            this.beat = (this.beat + 1) % this.totalBeats;
         }
-
     }
 })
 
-global.app = app
+global.sequencer = sequencer
+
+const player = new Tone.Player("https://tonejs.github.io/audio/berklee/gong_1.mp3").toDestination();
+
+/* VIEW */
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+
+var clock=null;
+slider.oninput = function() {
+    clearInterval(clock)
+    console.log(this.value)
+    sequencer.bpm = this.value
+    clock = setInterval(playBeat, 60000/sequencer.bpm)
+}
+
+/*CONTROLLER*/
+
+function playBeat() {
+    sequencer.nextBeat()
+    if(sequencer.keys[sequencer.beat]) { player.start() }
+}
+
+Tone.loaded().then(() => {        
+    console.log("Preset loaded")
+});
+
+document.querySelector("#butt1").onclick = function (){
+    console.log("pressed")
+    sequencer.totalBeats += 1;
+    sequencer.keys.push(true);
+
+    console.log(sequencer.totalBeats);
+}
+
+document.querySelector("#butt2").onclick = function (){
+    console.log("pressed")
+    sequencer.totalBeats -= 1;
+    sequencer.keys.pop();
+
+    console.log(sequencer.totalBeats)
+}
+
