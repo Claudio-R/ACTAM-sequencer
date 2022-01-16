437c87cf6a5d7c7632d935b186a586f004111daf
Marco Cafaro <marco2.cafaro@mail.polimi.it>
2022-01-16 23:21:18 +0100

    Added copy-bar functionality

--- src/script.js
+++ src/script.js
@@ -299,6 +299,11 @@ let keyComponent = {
             if(this.state1){ this.state1 = !this.state1 }
             if(this.state2){ this.state2 = !this.state2 }
             if(this.state3){ this.state3 = !this.state3 }
+        },
+        setKey(state1,state2,state3){
+            this.state1=state1
+            this.state2=state2
+            this.state3=state3
         }
     },
 
@@ -363,6 +368,25 @@ let columnComponent = {
         playInst3(keyId){
             drum[keyId].start();
         },
+        getKeyProps() {
+            var key_state1 = []
+            var key_state2 = []
+            var key_state3 = []
+            for(j=0;j<this.tonesInScale;j++){
+                key_state1[j]=this.$refs.keys_refs[j].state1;
+                key_state2[j]=this.$refs.keys_refs[j].state2;
+                key_state3[j]=this.$refs.keys_refs[j].state3;
+            }
+            return {key_state1,key_state2,key_state3}
+        },
+        setColumn(newvar){
+            key_state1=newvar.key_state1
+            key_state2=newvar.key_state2
+            key_state3=newvar.key_state3
+            for(j=0;j<this.tonesInScale;j++){
+                this.$refs.keys_refs[j].setKey(key_state1[j],key_state2[j],key_state3[j])
+            }
+        }
     }
 }
 
@@ -380,6 +404,7 @@ let layerComponent = {
             <div v-for="j in n_bars">\
                 <div class="keyboard">\
                     <column-component v-for="k in num_beats"\
+                        ref="columns_refs"\
                         class="column" :style="cssVars"\
                         :class="{playing : k*j-(k-num_beats)*(j-1) === isPlaying + 1}"\
                         :beatId="k*j-1-(k-num_beats)*(j-1)"\
@@ -553,6 +578,16 @@ let layerComponent = {
         lessOctave(){
             this.octave--
             this.keyboardCreator()
+        },
+        addLBar(){
+            Vue.nextTick(() =>{
+                //column_states = Array(this.num_beats)
+                    for(i=0;i<this.num_beats;i++) {
+                    newvar = this.$refs.columns_refs[i].getKeyProps()
+                    this.$refs.columns_refs[i+(this.n_bars-1)*this.num_beats].setColumn(newvar)
+                    //column_states[i]=(newvar)
+                    }
+            })
         }
     },
 };
@@ -565,7 +600,7 @@ let sequencerComponent = {
                 <p class="viewer">BPM: {{bpm}}</p>\
                 <p class="viewer">Selected instrument: {{inst_name[inst_id-1]}}</p>\
                 <p class="viewer">Bars: {{n_bars}}</p>\
-                <button id="remove-btn" @click="if(n_bars<4){n_bars++}"> + </button>\
+                <button id="remove-btn" @click="if(n_bars<4){n_bars++;addBar()}"> + </button>\
                 <button id="addKey-btn" @click="if(n_bars>1){n_bars--}"> - </button>\
             </div>\
             <controller-component\
@@ -664,6 +699,11 @@ let sequencerComponent = {
         instSelected(inst_id) {
             this.inst_id=inst_id
         },
+        addBar(){
+            for(idx in this.layers) {
+                this.$refs.layers_refs[idx].addLBar()
+            }
+        }
     }
 }
 
